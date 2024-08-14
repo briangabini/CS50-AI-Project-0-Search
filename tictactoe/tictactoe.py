86bb9774316276e09f0066576ea8bebe80346c6c
briangabini <briangabini@gmail.com>
2024-08-14 21:55:24 +0800

    feat: implement utility method

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -81,7 +81,7 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    return 1 if winner(board) == X else -1 if winner(board) == O else 0
 
 
 def minimax(board):
